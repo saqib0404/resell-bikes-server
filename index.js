03ce0c98a9beae29b3afbda9ee81b21d22450c7d
Saqib Ahmad <you@example.com>
2022-11-24 14:44:52 +0600

    users api added

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
+        const userCollection = client.db('resell-bikes').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const userInfo = await userCollection.insertOne(user);
+            res.send(userInfo);
+        })
     }
     finally { }
 }
